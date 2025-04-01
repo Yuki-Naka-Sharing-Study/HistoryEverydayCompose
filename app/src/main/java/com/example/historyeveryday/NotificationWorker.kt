7ba8b953c166af4f08e669d045269b19d83b798b
仲優樹 <yu-ki216m@outlook.jp>
2025-04-02 08:12:59 +0900

    Created NotificationWorker class.

---
+++ app/src/main/java/com/example/historyeveryday/NotificationWorker.kt
@@ -0,0 +1,12 @@
+package com.example.historyeveryday
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class NotificationWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
+    override fun doWork(): Result {
+        // 通知の送信ロジックをここに追加
+        return Result.success()
+    }
+}
