04463f7f620fb6d087bdd60a2840c359e359c970
仲優樹 <yu-ki216m@outlook.jp>
2025-04-02 20:06:14 +0900

    Modified HistoryEvent data class.

--- app/src/main/java/com/example/historyeveryday/HistoryEvent.kt
+++ app/src/main/java/com/example/historyeveryday/HistoryEvent.kt
@@ -1,6 +1,10 @@
 package com.example.historyeveryday
 
+import com.google.gson.annotations.SerializedName
+
 data class HistoryEvent(
     val date: String,
-    val events: List<String>
+    val event: String,
+    val description: String,
+    @SerializedName ("image_url") val imageUrl: String
 )
