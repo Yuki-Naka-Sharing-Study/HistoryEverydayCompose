5d0f6638b429e0e4d8d309d74a49e90c9d783c6e
仲優樹 <yu-ki216m@outlook.jp>
2025-04-02 20:17:23 +0900

    Modified HistoryScreen and HistoryViewModel.

--- app/src/main/java/com/example/historyeveryday/HistoryScreen.kt
+++ app/src/main/java/com/example/historyeveryday/HistoryScreen.kt
@@ -1,15 +1,21 @@
 package com.example.historyeveryday
 
 import android.content.Context
+import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
 
 @Composable
 fun HistoryScreen(viewModel: HistoryViewModel = viewModel(), context: Context) {
@@ -30,10 +36,43 @@ fun HistoryScreen(viewModel: HistoryViewModel = viewModel(), context: Context) {
         Spacer(modifier = Modifier.height(32.dp))
 
         events.forEach { event ->
-            Text(
-                text = event,
-                style = MaterialTheme.typography.bodyMedium
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                val imageUrl = event.imageUrl
+
+                AsyncImage(
+                    model = ImageRequest
+                        .Builder(LocalContext.current)
+                        .data(imageUrl)
+//                        .size(Size.ORIGINAL)
+                        .crossfade(true)
+                        .build(),
+                    placeholder = painterResource(com.example.historyeveryday.R.drawable.ic_launcher_background),
+                    error = painterResource(com.example.historyeveryday.R.drawable.error),
+                    contentDescription = "Profile Image",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp),
+                    contentScale = ContentScale.Crop,
+                )
+                Text(
+                    text = event.event,
+                    style = MaterialTheme.typography.headlineSmall
+                )
+                Text(
+                    text = event.description,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Log.d("HistoryScreen", "Image URL: ${event.imageUrl}")
+
+                Spacer(modifier = Modifier.height(32.dp))
+            }
         }
     }
 }
--- app/src/main/java/com/example/historyeveryday/HistoryViewModel.kt
+++ app/src/main/java/com/example/historyeveryday/HistoryViewModel.kt
@@ -8,12 +8,11 @@ import java.util.*
 class HistoryViewModel : ViewModel() {
     private val historyEvents = mutableListOf<HistoryEvent>()
 
-    fun getEventsForToday(context: Context): List<String> {
+    fun getEventsForToday(context: Context): List<HistoryEvent> {
         if (historyEvents.isEmpty()) {
             historyEvents.addAll(loadHistoryEvents(context))
         }
-
         val today = SimpleDateFormat("MM-dd", Locale.getDefault()).format(Date())
-        return historyEvents.firstOrNull { it.date == today }?.events ?: emptyList()
+        return historyEvents.filter { it.date == today }.orEmpty()
     }
 }
